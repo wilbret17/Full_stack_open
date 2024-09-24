be3b4ff6eea144d54577b09c7577c9ea1b39360a
wilbret17 <wille.nordell@gmail.com>
2024-09-24 13:45:18 +0300

    PARTS 1.1-1.5 all done

---
+++ src/App.jsx
@@ -0,0 +1,52 @@
+const Header = ({ courseName }) => <h1>{courseName}</h1>
+
+const Part = ({ part }) => (
+  <p>{part.name} {part.exercises}</p>
+)
+
+const Content = ({ parts }) => (
+  <div>
+    <Part part={parts[0]} />
+    <Part part={parts[1]} />
+    <Part part={parts[2]} />
+  </div>
+)
+
+const Total = ({ parts }) => {
+  const totalExercises = parts[0].exercises + parts[1].exercises + parts[2].exercises
+
+  return (
+    <p>Number of exercises {totalExercises}</p>
+  )
+}
+
+const App = () => {
+  const course = {
+    name: 'Half Stack application development',
+    parts: [
+      {
+        name: 'Fundamentals of React',
+        exercises: 10
+      },
+      {
+        name: 'Using props to pass data',
+        exercises: 7
+      },
+      {
+        name: 'State of a component',
+        exercises: 14
+      }
+    ]
+  }
+
+  return (
+    <div>
+      <Header courseName={course.name} />
+      <Content parts={course.parts} />
+      <Total parts={course.parts} />
+    </div>
+  )
+}
+
+export default App
+
