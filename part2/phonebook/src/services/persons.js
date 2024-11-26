f7b4f98d8036890aff77546d8f59af5068bcd86f
wilbret17 <wille.nordell@gmail.com>
2024-11-26 12:15:04 +0200

    Update persons.js

--- part2/phonebook/src/services/persons.js
+++ part2/phonebook/src/services/persons.js
@@ -1,6 +1,6 @@
 import axios from 'axios'
 
-const baseUrl = 'http://localhost:3001/api/persons'
+const baseUrl = 'https://fullstack3-gahg.onrender.com/api/persons';
 
 const getAll = () => {
   return axios.get(baseUrl).then(response => response.data);
