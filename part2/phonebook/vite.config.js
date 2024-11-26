a3fb6fe66bd4c7db0f968bd345e88e09c3f12665
wilbret17 <wille.nordell@gmail.com>
2024-11-26 13:45:00 +0200

    updated

--- part2/phonebook/vite.config.js
+++ part2/phonebook/vite.config.js
@@ -4,4 +4,8 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    host: '0.0.0.0',  // Ensure Vite listens on all network interfaces
+    port: process.env.PORT || 3000,  // Use Render's provided port or default to 3000
+  }
 })
